9881b9620a33b0d809b6bacaba412043118f7d2c
rpkkumar <35652674+rpkkumar@users.noreply.github.com>
2018-01-25 16:38:10 +0530

    Create r.c

---
+++ r.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+viod main()
+{
+clrscr();
+int b=[a],[e],[i],[o],[u];
+printf("Enter a letter");
+scanf("%d",&b);
+if (b=[a],[e],[i],[o],[u])
+printf("This is vowel letter");
+else
+printf("This is consonent letter");
+getch();
+}
